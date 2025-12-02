2f9f7a94947ee08339f8aaeaa271cf28699fbf67
stellamercado-spec <stella.mercado@discoverystudent.org>
2025-12-02 11:29:06 -0500

    stella

--- Assets/scripts/playercontroller.cs
+++ Assets/scripts/playercontroller.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class playercontroller : MonoBehaviour
 {
+    private Rigidbdy rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+   rb = GetComponent <Rigidbody>();
     }
-
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
         
     }
+void OnMove (InputValue movementValue)
+ {
+  Vector2 movementVector= movementValue.Get<Vector2>();  
+ }  
 }
